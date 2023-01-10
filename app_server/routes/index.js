ad6f2c00d6cd548771fb7ac10e921e62759c12a2
Santiagor2230 <52907423+Santiagor2230@users.noreply.github.com>
2023-01-10 03:47:53 -0500

    module 2

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const controller = require("../controllers/main");
 
 /* GET home page. */
+console.log("Inside app_server, routes, index.js.");
 router.get("/", controller.index);
 
 module.exports = router;
