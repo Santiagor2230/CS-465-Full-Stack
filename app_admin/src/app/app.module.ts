eb04dad7478e6681fe154956e6669d455982e371
Santiagor2230 <52907423+Santiagor2230@users.noreply.github.com>
2023-02-16 21:59:41 -0500

    module7

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { HttpModule } from "@angular/http";
+//import { HttpModule } from "@angular/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-router.module";
@@ -9,7 +10,10 @@ import { TripListingComponent } from "./trip-listing/trip-listing.component";
 import { TripCardComponent } from "./trip-card/trip-card.component";
 import { TripDataService } from "./services/trip-data.service";
 import { AddTripComponent } from "./add-trip/add-trip.component";
-import { EditTripComponent } from './edit-trip/edit-trip.component';
+import { EditTripComponent } from "./edit-trip/edit-trip.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
   declarations: [
@@ -18,10 +22,14 @@ import { EditTripComponent } from './edit-trip/edit-trip.component';
     TripCardComponent,
     AddTripComponent,
     EditTripComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    //HttpModule,
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
--- app_api/database/db.js
+++ app_api/database/db.js
@@ -70,3 +70,4 @@ process.on("SIGTERM", () => {
 connect();
 
 require("./models/travlr");
+require("./models/user");
